3afdcd4034c5dc29b280a3b52d4b3eea555b40fa
benmcm1994 <mcmullanbenjamin@gmail.com>
2019-07-09 11:47:09 +0100

    Added cards

--- source/index.blade.php
+++ source/index.blade.php
@@ -3,14 +3,12 @@
 @section('body')
     <section id="quote-section">
         <div class="container" id="quote-header">
-            <div class="jumbotron">
+            <div class="jumbotron bg-light">
                 <div class="row">
                     <div class="col-12 col-lg-5 text-column">
-                        <h1>Grow your business with extra credit</h1>
-                        <p class="lead">Everyday we help businesses like yours expand by connecting you to suitable
-                            lenders that
-                            meet
-                            your business goals.</p>
+                        <h1>Get the right funding for your business</h1>
+                        <p class="lead">Everyday we help lots of businesses just like yours get the funding they need.
+                            From start-ups to well established companies, we can help your business grow.</p>
                         <a class="btn btn-success btn-lg" href="#" role="button">Get Your Free Quote</a>
                     </div>
                     <div class="col-0 col-lg-7 img-column">
@@ -18,10 +16,67 @@
                     </div>
                 </div>
             </div>
+            <div class="row">
+                <div class="col text-center" id="quote-card-header">
+                    <h3>A transparent service that leaves you in control</h3>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12 col-md-3">
+                    <div class="card usp-card">
+                        <div class="card-body">
+                            <i class="fa fa-tag fa-5x pb-3 text-primary" aria-hidden="true"></i>
+                            <h5><strong>100% FREE TO USE</strong></h5>
+                            <p>We don’t charge a penny for using our online service.</p>
+                        </div>
+                    </div>
+                </div>
+                <br>
+                <div class="col-12 col-md-3">
+                    <div class="card usp-card">
+                        <div class="card-body">
+                            <i class="fa fa-list-ol fa-5x pb-3 text-primary" aria-hidden="true"></i>
+                            <h5><strong>MULTIPLE FUNDERS</strong></h5>
+                            <p>Get matched to funders that can help your business.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-3">
+                    <div class="card usp-card">
+                        <div class="card-body">
+                            <i class="fa fa-clock-o fa-5x pb-3 text-primary" aria-hidden="true"></i>
+                            <h5><strong>NO PRESSURE</strong></h5>
+                            <p>Decide in your own time. There is absolutely no pressure.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-3">
+                    <div class="card usp-card">
+                        <div class="card-body">
+                            <i class="fa fa-handshake-o fa-5x pb-3 text-primary" aria-hidden="true"></i>
+                            <h5><strong>TRUSTED PROVIDERS</strong></h5>
+                            <p>We partner with trusted UK business providers.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
-    <section id="usp-section">
+    <section id="connect-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-12"><h3 class="text-center">We help connect your business to the right providers</h3>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-6">
+                    <img src="https://www.capalona.co.uk/include/images/step-1.png">
+                </div>
+                <div class="col-6">
 
+                </div>
+            </div>
+        </div>
     </section>
     <section id="loan-type-section">
         <div class="container" id="quote-header">
