48cbe164e4dc47ea217f6ed5707667ceec01a51c
benmcm1994 <mcmullanbenjamin@gmail.com>
2019-07-11 14:40:28 +0100

    Site updated

--- source/_partials/footer.blade.php
+++ source/_partials/footer.blade.php
@@ -1,39 +1,28 @@
-<footer class="pt-4 pt-md-5 shadow-lg border-top bg-primary">
+<footer class="shadow-lg border-top bg-primary p-5 m-0">
     <div class="container">
         <div class="row">
-            <div class="col-12 col-md">
-                <img class="mb-2" src="/assets/images/spurta-white.png" alt="" width="150"
+            <div class="col-12">
+                <img class="mb-5" src="/assets/images/spurta-white.png" alt="" width="150"
                      height="50">
-                <small class="d-block mb-3 text-white">&copy; 2019</small>
             </div>
-            <div class="col-6 col-md">
-                <h5>Features</h5>
-                <ul class="list-unstyled text-small">
-                    <li><a class="text-white" href="#">Cool stuff</a></li>
-                    <li><a class="text-white" href="#">Random feature</a></li>
-                    <li><a class="text-white" href="#">Team feature</a></li>
-                    <li><a class="text-white" href="#">Stuff for developers</a></li>
-                    <li><a class="text-white" href="#">Another one</a></li>
-                    <li><a class="text-white" href="#">Last time</a></li>
-                </ul>
-            </div>
-            <div class="col-6 col-md">
-                <h5>Resources</h5>
-                <ul class="list-unstyled text-small">
-                    <li><a class="text-white" href="#">Resource</a></li>
-                    <li><a class="text-white" href="#">Resource name</a></li>
-                    <li><a class="text-white" href="#">Another resource</a></li>
-                    <li><a class="text-white" href="#">Final resource</a></li>
-                </ul>
-            </div>
-            <div class="col-6 col-md">
-                <h5>About</h5>
-                <ul class="list-unstyled text-small">
-                    <li><a class="text-white" href="#">Team</a></li>
-                    <li><a class="text-white" href="#">Locations</a></li>
-                    <li><a class="text-white" href="#">Privacy</a></li>
-                    <li><a class="text-white" href="#">Terms</a></li>
-                </ul>
+            <div class="col-12 text-white">
+                <p><b>Disclaimer</b>: SpurtaFinance.co.uk is a trading name of Sorodo Limited. We help connect UK
+                    businesses
+                    access business finance providers, working directly with businesses and their advisors. We are a
+                    licensed credit broker and do not provide finance ourselves. Applicants must be aged 18 and over,
+                    Terms and conditions apply. All finance and quotes are subject to status and income. Guarantees and
+                    Indemnities may be required. Capalona can introduce applicants to a number of business finance
+                    providers based on the applicants' circumstances and creditworthiness. Capalona does not charge
+                    customers a fee for using its broker service, but receives a commission from lenders or other
+                    brokers for effecting such introductions.</p>
+                <p>Sorodo Limited is registered in England and Wales under company number 08039501. Registered office:
+                    Suite 3, Broncoed House, Broncoed Business Park, Wrexham Road, Mold, Flintshire, CH7 1HP, UK.</p>
+                <p>Sorodo Limited is authorised and regulated by the <a
+                            href="https://register.fca.org.uk/ShPo_FirmDetailsPage?id=001b000003jLyrmAAC"
+                            target="_blank" rel="noopener">Financial Conduct Authority</a> under firm reference number
+                    774781.</p>
+                <p>Sorodo Limited is registered on the Register of Data Controllers with the Information Commissioner's
+                    Office number ZA160887.</p>
             </div>
         </div>
     </div>
