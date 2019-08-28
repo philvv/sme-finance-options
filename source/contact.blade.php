929875ff4749bf1a0d2641e5cdc6aab96ae67279
benmcm1994 <mcmullanbenjamin@gmail.com>
2019-08-28 13:48:51 +0100

    Testing jquery form

--- source/contact.blade.php
+++ source/contact.blade.php
@@ -65,4 +65,18 @@
             </div>
         </div>
     </section>
+
+    <script>
+
+        $("#contact-form").submit(function(e) {
+
+            e.preventDefault();
+
+            var $form = $(this);
+            $.post($form.attr("action"), $form.serialize()).then(function() {
+                alert("Thank you!");
+            });
+        });
+
+    </script>
 @endsection
